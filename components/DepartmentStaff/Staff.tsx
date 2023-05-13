6b6152bf6f0ca9f212b3c69e01c800f5b8523aa2
vladblip <ezio2506@gmail.com>
2023-05-13 17:33:53 +0300

    Added detailed view and removed unnecessary code

--- components/DepartmentStaff/Staff.tsx
+++ components/DepartmentStaff/Staff.tsx
@@ -1,30 +1,64 @@
-import ButtonOutline from "../ButtonOutline/ButtonOutline";
+import { useState } from "react";
+
 import Image from "next/image";
+
+import StaffDetails from "./StaffDetails";
+import ButtonOutline from "../ButtonOutline/ButtonOutline";
 
 import styles from "./Staff.module.scss";
 
 type StaffProps = {
-  img: string;
   name: string;
+  img: string;
   description: string;
-  url: string;
+  workExperience: string;
+  accomplishments: string;
+  curriculum: Array<String>;
+  scienceProfiles: Array<object>;
+  url?: string;
 };
 
-export default function Staff({ img, name, description, url }: StaffProps) {
+export default function Staff({
+  name,
+  img,
+  description,
+  workExperience,
+  accomplishments,
+  curriculum,
+  scienceProfiles,
+}: StaffProps) {
+  const [active, setActive] = useState(false);
+
+  const onClickHandler = () => setActive((prevActive) => !prevActive);
+
   return (
-    <div className={styles.container}>
-      <Image
-        className={styles.img}
-        src={img}
-        width={260}
-        height={300}
-        alt={`${name} photo`}
-      />
-      <h3 className="h-3">{name}</h3>
-      <p className={styles.description}>{description}</p>
-      <ButtonOutline type="link" href={url}>
-        Докладніше
-      </ButtonOutline>
-    </div>
+    <>
+      <div className={styles.container}>
+        <Image
+          className={styles.img}
+          src={img}
+          width={260}
+          height={300}
+          alt={`${name} photo`}
+        />
+        <h3 className="h-3">{name}</h3>
+        <p className={styles.description}>{description}</p>
+        <ButtonOutline type="button" onClick={onClickHandler}>
+          Докладніше
+        </ButtonOutline>
+      </div>
+      {active ? (
+        <StaffDetails
+          name={name}
+          img={img}
+          description={description}
+          workExperience={workExperience}
+          accomplishments={accomplishments}
+          curriculum={curriculum}
+          scienceProfiles={scienceProfiles}
+          onClick={onClickHandler}
+        />
+      ) : null}
+    </>
   );
 }
