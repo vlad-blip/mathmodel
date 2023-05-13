c7d8be3d166759a54caab76d625be5aec2c21980
vladblip <ezio2506@gmail.com>
2023-05-13 17:31:43 +0300

    Added slider

--- components/DepartmentStaff/StaffList.tsx
+++ components/DepartmentStaff/StaffList.tsx
@@ -1,50 +1,59 @@
-import Staff from "./Staff";
-import ButtonSlider from "../ButtonSlider/ButtonSlider";
+import staffData from "@/data/staff.json";
 
-import styles from "./StaffList.module.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import Staff from "./Staff";
 import ButtonOutline from "../ButtonOutline/ButtonOutline";
 
+import styles from "./StaffList.module.scss";
+
 export default function StaffList() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1440 },
+      items: 4,
+    },
+    laptop: {
+      breakpoint: { max: 1440, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 544 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 544, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <ButtonSlider direction="left" onClick={() => {}} />
-        <ul className={styles.list}>
-          <li>
+        <Carousel
+          responsive={responsive}
+          ssr={true}
+          infinite={true}
+          containerClass={styles.list}
+        >
+          {staffData.map((staff, i) => (
             <Staff
-              name="Парфьонова Тетяна Олександрівна"
-              description="доцент кафедри, кандидат фізико-математичних наук 
-заступник завідувача кафедри"
-              url="/13123"
-              img="/Staff/parfenova.png"
+              key={i}
+              img={staff.img}
+              name={staff.name}
+              description={staff.description}
+              accomplishments={staff.accomplishments}
+              curriculum={staff.curriculum}
+              workExperience={staff.workExperience}
+              scienceProfiles={staff.scienceProfiles}
             />
-          </li>
-          <li>
-            <Staff
-              name="Ольховська Олена Володимирівна"
-              description="завідувач кафедри, кандидат фізико-математичних наук"
-              url="/13123"
-              img="/Staff/olkhovska.png"
-            />
-          </li>
-          <li>
-            <Staff
-              name="Черненко Оксана Олексіївна"
-              description="доцент кафедри, кандидат фізико-математичних наук"
-              url="/13123"
-              img="/Staff/chernenko.png"
-            />
-          </li>
-          <li>
-            <Staff
-              name="Ольховський Дмитро Миколайович"
-              description="доцент кафедри, кандидат фізико-математичних наук"
-              url="/13123"
-              img="/Staff/olkhovsky.png"
-            />
-          </li>
-        </ul>
-        <ButtonSlider direction="right" onClick={() => {}} />
+          ))}
+        </Carousel>
       </div>
       <ButtonOutline type="button">Весь колектив</ButtonOutline>
     </div>
