571ce74fb22188502c6cf9017aa07ae1711146c0
vladblip <ezio2506@gmail.com>
2023-05-13 21:14:36 +0300

    Added infinite carousel

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -1,4 +1,4 @@
-import ButtonSlider from "../ButtonSlider/ButtonSlider";
+import "react-multi-carousel/lib/styles.css";
 
 import styles from "./Partners.module.scss";
 
@@ -9,17 +9,129 @@ type PartnersProps = {
 export default function Partners({ children }: PartnersProps) {
   return (
     <div className={styles.container}>
-      <ButtonSlider direction="left" onClick={() => {}} />
-      {children && Array.isArray(children) ? (
-        <ul className={styles.list}>
-          {children.map((child, i) => (
-            <li key={i}>{child}</li>
-          ))}
-        </ul>
-      ) : (
-        <div>{children}</div>
-      )}
-      <ButtonSlider direction="right" onClick={() => {}} />
+      <div className={styles["slide-track"]}>
+        {children && Array.isArray(children) ? (
+          <>
+            {children.map((child, i) => (
+              <div key={i} className={styles.slide}>
+                {child}
+              </div>
+            ))}
+            {children.map((child, i) => (
+              <div key={i} className={styles.slide}>
+                {child}
+              </div>
+            ))}
+          </>
+        ) : (
+          <div>{children}</div>
+        )}
+      </div>
     </div>
   );
 }
+
+/* <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div className={styles.slide}>
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+                height="100"
+                width="250"
+                alt=""
+              /> 
+              </div>*/
