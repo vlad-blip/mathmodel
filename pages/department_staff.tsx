deef8777d94bf894b2fa6452b8beb171117b9f7a
vladblip <ezio2506@gmail.com>
2023-05-14 20:51:02 +0300

    New page for department staff

---
+++ pages/department_staff.tsx
@@ -0,0 +1,20 @@
+import staffData from "@/data/staff.json";
+
+import StaffLargeList from "@/components/DepartmentStaff/StaffLargeList";
+
+import styles from "@/styles/Department_staff.module.scss";
+
+export default function DepartmentStaff() {
+  return (
+    <main className="main">
+      <div className={`container`}>
+        <h1 className="h-1 page-heading">Склад кафедри</h1>
+        <StaffLargeList data={staffData.current} />
+        <h1 className={`${styles.honorable_heading} h-1`}>
+          Почесні працівники кафедри
+        </h1>
+        <StaffLargeList data={staffData.honorable} />
+      </div>
+    </main>
+  );
+}
