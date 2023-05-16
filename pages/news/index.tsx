2ca1d72ace26005a166883b060939744b3d66dfb
vladblip <ezio2506@gmail.com>
2023-05-16 11:16:44 +0300

    News page

---
+++ pages/news/index.tsx
@@ -0,0 +1,9 @@
+export default function News() {
+  return (
+    <main className="main">
+      <div className="container">
+        <h1 className="h-1 page-heading">Новини</h1>
+      </div>
+    </main>
+  );
+}
