54143f684e5c393fecd9f69f5859d93cfdc0eff2
vladblip <ezio2506@gmail.com>
2023-08-22 19:52:19 +0300

    * added filter container

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,8 +1,21 @@
+import Search from "@/components/Search/Search";
+import ButtonSort from "@/components/ButtonSort/ButtonSort";
+import Categories from "@/components/Categories/Categories";
+
+import styles from "@/styles/News.module.scss";
+
 export default function News() {
   return (
     <main className="main">
       <div className="container">
         <h1 className="h-1 page-heading">Новини</h1>
+        <div className={styles.filter}>
+          <div>
+            <Search placeholder="Пошук новини" />
+            <ButtonSort />
+          </div>
+          <Categories />
+        </div>
       </div>
     </main>
   );
